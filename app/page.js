cdf7a634965e3e8263d058bc64e594205fa518d2
Aneesh kumar maury <aneeshmaury.in@gmail.com>
2024-05-05 15:10:41 +0530

    wwww

--- app/page.js
+++ app/page.js
@@ -3,12 +3,14 @@ import "./globals.css";
 
 export default function Home() {
   return (
-    <div className=" px-9">
-       <div className="main ">
+    <div className=" px-5 mt-6">
+    <p className="font-bold mb-2">Complaint</p>
+       <div className="main p-4  shadow-md">
+       <p className='text-sm mb-2 mt-[-7px]'>Transaction</p>
 <div class="max-w-2xl mx-auto">
 
 <label for="countries" class="block text-sm  text-gray-400 dark:text-gray-400">Nature of transaction</label>
-      <select id="countries" class=" border-b bg-transparent text-sm    block w-full p-2.5   dark:placeholder-gray-400 dark:text-black ">
+      <select id="countries" class=" border-b bg-transparent text-sm    block w-full py-1   dark:placeholder-gray-400 dark:text-black ">
       <option selected>Select Nature of transaction</option>
       <option value="US">Person to person</option>
       <option value="CA">Person to merchent</option>

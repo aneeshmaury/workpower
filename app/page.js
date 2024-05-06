6be672be84069e432c2a6dc362a989e91f0e5750
Aneesh kumar maury <aneeshmaury.in@gmail.com>
2024-05-06 15:34:25 +0530

    dsdlkahl

--- app/page.js
+++ app/page.js
@@ -47,16 +47,133 @@ export default function Home() {
 <div class="max-w-2xl mx-auto mt-4">
 
 <label for="countries" class="block text-[14px] tracking-wide text-[#27357e]">Comment</label>
-      <select id="countries" class=" border-b bg-transparent text-[15px] tracking-wide   block w-full py-1   dark:placeholder-gray-400 dark:text-black ">
+      <div id="countries" class=" border-b bg-transparent text-[15px] tracking-wide   block w-full py-1   dark:placeholder-gray-400 dark:text-black ">
      <input/>
      
-  </select>
+  </div>
 
 <p class=" text-[14px] text-[#313131] italic tracking-wide">Provide brief about your issue. Not greater than 500 characters
   
 </p>
 
 </div>
+<div class="max-w-2xl mx-auto mt-4">
+
+<label for="countries" class="block text-[14px] tracking-wide text-[#27357e]">Transaction ID</label>
+      <div id="countries" class=" border-b bg-transparent text-[15px] tracking-wide   block w-full py-1   dark:placeholder-gray-400 dark:text-black ">
+     <input/>
+     
+  </div>
+
+<p class=" text-[14px] text-[#313131] italic tracking-wide">Please enter your transaction reference number. It starts with 1, 7, 8, 9 or 0
+  
+</p>
+
+</div>
+
+<div class="max-w-2xl mx-auto">
+
+<label for="countries" class="block text-[14px] tracking-wide text-[#27357e]">Bank Name</label>
+      <select id="countries" class=" border-b bg-transparent text-[15px] tracking-wide   block w-full py-1   dark:placeholder-gray-400 dark:text-black ">
+      <option selected>--Select Bank Name--</option>
+      <option value="US">AU Small Finance Bank</option>
+      <option value="CA">Person to merchent</option>
+     
+  </select>
+
+<p class=" text-[14px] text-[#313131] italic tracking-wide">Please select the issue from drop down list
+  
+</p>
+</div>
+<div class="max-w-2xl mx-auto mt-4">
+
+<label for="countries" class="block text-[14px] tracking-wide text-[#27357e]">UPI ID</label>
+      <div id="countries" class=" border-b bg-transparent text-[15px] tracking-wide   block w-full py-1   dark:placeholder-gray-400 dark:text-black ">
+     <input/>
+     
+  </div>
+
+<p class=" text-[14px] text-[#313131] italic tracking-wide">Please enter your UPI ID
+  
+</p>
+
+</div>
+<div class="max-w-2xl mx-auto mt-4">
+
+<label for="countries" class="block text-[14px] tracking-wide text-[#27357e]">Amount</label>
+      <div id="countries" class=" border-b bg-transparent text-[15px] tracking-wide   block w-full py-1   dark:placeholder-gray-400 dark:text-black ">
+     <input/>
+     
+  </div>
+
+<p class=" text-[14px] text-[#313131] italic tracking-wide">Please capture rupee & paise separately in the box provided
+  
+</p>
+
+</div>
+
+<div class="max-w-2xl mx-auto mt-4">
+
+<label for="countries" class="block text-[14px] tracking-wide text-[#27357e]">Date of Transaction</label>
+      <div id="countries" class=" border-b bg-transparent text-[15px] tracking-wide   block w-full py-1   dark:placeholder-gray-400 dark:text-black ">
+     <input/>
+     
+  </div>
+
+<p class=" text-[14px] text-[#313131] italic tracking-wide">Please use the calendar to select the date
+  
+</p>
+
+</div>
+<div class="max-w-2xl mx-auto mt-4">
+
+<label for="countries" class="block text-[14px] tracking-wide text-[#27357e]">Email ID</label>
+      <div id="countries" class=" border-b bg-transparent text-[15px] tracking-wide   block w-full py-1   dark:placeholder-gray-400 dark:text-black ">
+     <input/>
+     
+  </div>
+
+<p class=" text-[14px] text-[#313131] italic tracking-wide">Please enter your mail ID
+  
+</p>
+
+</div>
+<div class="max-w-2xl mx-auto mt-4">
+
+<label for="countries" class="block text-[14px] tracking-wide text-[#27357e]">Registered Mobile No</label>
+      <div id="countries" class=" border-b bg-transparent text-[15px] tracking-wide   block w-full py-1   dark:placeholder-gray-400 dark:text-black ">
+     <input/>
+     
+  </div>
+
+<p class=" text-[14px] text-[#313131] italic tracking-wide">Please enter your 10 digit mobile number. Do not prefix with +91 or 0
+  
+</p>
+
+</div>
+<div class="max-w-2xl mx-auto mt-4">
+
+<label for="countries" class="block text-[14px] tracking-wide text-[#27357e]">Attach your updated bank account statement</label>
+     
+     
+      <div id="countries" class=" border-b bg-transparent text-[15px] tracking-wide   block w-full py-1   dark:placeholder-gray-400 dark:text-black ">
+    <div className='flex items-center gap-3'>
+<div className='bg-zinc-100 p-2'>Choose file</div>
+<div>Bank statement.jpg</div>
+    </div>
+
+     <input className='mt-[-15px]'/>
+     
+  </div>
+
+<p class=" text-[14px] text-[#313131] italic tracking-wide">Please enter your 10 digit mobile number. Do not prefix with +91 or 0
+  
+</p>
+
+</div>
+
+
+
 
 
 
